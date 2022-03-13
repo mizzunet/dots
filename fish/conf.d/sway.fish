7add86acfbda9959680abea1acc196f5ef2d9bb8
Missu <mizzunet@protonmail.com>
2022-03-13 21:24:13 +0530

    Inside we both know what's been going on

--- fish/conf.d/sway.fish
+++ fish/conf.d/sway.fish
@@ -1,3 +1,15 @@
-## If running from tty1 start sway
+# Autostart sway when user login on tty1
+
+set -x XDG_CURRENT_DESKTOP 'sway' # xdg-desktop-portal
+set -x MOZ_ENABLE_WAYLAND 1
+set -x KITTY_ENABLE_WAYLAND 1
+set -x QT_QPA_PLATFORM 'wayland-egl'
+set -x BROWSER 'firefox'
+
 set TTY1 (tty)
-[ "$TTY1" = "/dev/tty1" ] && exec sway -d 2> /tmp/sway.log
+
+if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
+  # Debug
+  #exec dbus-run-session sway -d 2> ~/sway.log
+  exec dbus-run-session sway
+end
