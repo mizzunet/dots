43c13ce24332afc981a492f17299dde276c71b3b
Missu <mizzunet@protonmail.com>
2022-05-17 03:02:30 +0530

    push

--- waydroid.sh
+++ waydroid.sh
@@ -13,12 +13,12 @@ yay -S linux-zen
 yay -S linux-xanmod
 
 # DOWNLOAD IMAGES
-set BROWSER "chromium"
+set BROWSER chromium
 $BROWSER "" # SYSTEM
 $BROWSER "" # VENDOR
 
 # UNZIP & MOVE TO IMAGES DIRECOTRY
-set IMAGE_DIR "/usr/share/waydroid-extra/images/"
+set IMAGE_DIR /usr/share/waydroid-extra/images/
 unzip *.zip
 mkdir -p $IMAGE_DIR 
 mv *.img $IMAGE_DIR
@@ -47,37 +47,41 @@ doas bash -c 'echo debug.sf.nobootanimation=1 >> /var/lib/waydroid/waydroid_base
 # CONNECT ADB
 adb connect 192.168.250.112:5555
 
-set OVERLAY "adb shell cmd overlay"
 # LOOK
+set OVERLAY "adb shell cmd overlay"
 $OVERLAY enable org.lineageos.overlay.customization.blacktheme
 $OVERLAY enable com.android.theme.color.black
 $OVERLAY enable com.android.theme.icon_pack.circular.android
 $OVERLAY enable org.lineageos.overlay.customization.blacktheme
-$OVERLAY enable com.android.theme.icon_pack.circular.settingsset UNINSTALL="adb shell pm uninstall --user 0"
+$OVERLAY enable com.android.theme.icon_pack.circular.settingsset
+$OVERLAY enable org.lineageos.overlay.font.rubik 
 
 # DISABLE BLOATS
 ## USE GUI https://github.com/0x192/universal-android-debloater
+set UNINSTALL "adb shell pm uninstall --user 0"
+$UNINSTALL android.ext.services
+$UNINSTALL android.ext.shared
 $UNINSTALL com.android.backupconfirm
 $UNINSTALL com.android.bips
 $UNINSTALL com.android.bookmarkprovider
 $UNINSTALL com.android.calculator2
 $UNINSTALL com.android.camera2
 $UNINSTALL com.android.captiveportallogin
+$UNINSTALL com.android.certinstaller
 $UNINSTALL com.android.companiondevicemanager
 $UNINSTALL com.android.contacts
+$UNINSTALL com.android.cts.ctsshim
 $UNINSTALL com.android.cts.priv.ctsshim
 $UNINSTALL com.android.deskclock
-$UNINSTALL com.android.documentsui
 $UNINSTALL com.android.dreams.basic
 $UNINSTALL com.android.dreams.phototable
+$UNINSTALL com.android.dynsystem
 $UNINSTALL com.android.egg
 $UNINSTALL com.android.email
-$UNINSTALL android.ext.services
 $UNINSTALL com.android.exchange
 $UNINSTALL com.android.gallery3d
 $UNINSTALL com.android.hotspot2
 $UNINSTALL com.android.htmlviewer
-$UNINSTALL com.android.inputdevices
 $UNINSTALL com.android.inputmethod.latin
 $UNINSTALL com.android.internal.display.cutout.emulation.corner
 $UNINSTALL com.android.internal.display.cutout.emulation.double
@@ -88,54 +92,62 @@ $UNINSTALL com.android.internal.systemui.navbar.gestural_narrow_back
 $UNINSTALL com.android.internal.systemui.navbar.gestural_wide_back
 $UNINSTALL com.android.internal.systemui.navbar.threebutton
 $UNINSTALL com.android.internal.systemui.navbar.twobutton
+$UNINSTALL com.android.keychain
+$UNINSTALL com.android.launcher3
+$UNINSTALL com.android.localtransport
+$UNINSTALL com.android.location.fused
 $UNINSTALL com.android.managedprovisioning
+$UNINSTALL com.android.mms.service
+$UNINSTALL com.android.modulemetadata
+$UNINSTALL com.android.mtp
 $UNINSTALL com.android.networkstack.permissionconfig
+$UNINSTALL com.android.onetimeinitializer
 $UNINSTALL com.android.pacprocessor
+$UNINSTALL com.android.phone
 $UNINSTALL com.android.printservice.recommendation
 $UNINSTALL com.android.printspooler
 $UNINSTALL com.android.providers.blockednumber
 $UNINSTALL com.android.providers.calendar
-$UNINSTALL com.android.providers.contacts
-$UNINSTALL com.android.providers.media
+$UNINSTALL com.android.providers.telephony
 $UNINSTALL com.android.providers.userdictionary
+$UNINSTALL com.android.provision
 $UNINSTALL com.android.proxyhandler
 $UNINSTALL com.android.se
+$UNINSTALL com.android.server.telecom
+$UNINSTALL com.android.settings.intelligence
 $UNINSTALL com.android.sharedstoragebackup
 $UNINSTALL com.android.simappdialog
 $UNINSTALL com.android.smspush
+$UNINSTALL com.android.terminal
+$UNINSTALL com.android.theme.icon_pack.filled.android
+$UNINSTALL com.android.theme.icon_pack.filled.launcher
+$UNINSTALL com.android.theme.icon_pack.filled.settings
+$UNINSTALL com.android.theme.icon_pack.filled.systemui
+$UNINSTALL com.android.theme.icon_pack.filled.themepicker
 $UNINSTALL com.android.theme.icon_pack.rounded.themepicker
 $UNINSTALL com.android.traceur
 $UNINSTALL com.android.vpndialogs
+$UNINSTALL com.android.wallpaper
 $UNINSTALL com.android.wallpaper.livepicker
 $UNINSTALL com.android.wallpaperbackup
 $UNINSTALL com.android.wallpapercropper
-$UNINSTALL com.android.launcher3
-
 $UNINSTALL com.stevesoltys.seedvault
+$UNINSTALL org.lineageos.backgrounds
+$UNINSTALL org.lineageos.customization
 $UNINSTALL org.lineageos.eleven
 $UNINSTALL org.lineageos.etar
-$UNINSTALL org.lineageos.overlay.accent.black
-$UNINSTALL org.lineageos.overlay.accent.blue
-$UNINSTALL org.lineageos.overlay.accent.brown
-$UNINSTALL org.lineageos.overlay.accent.cyan
-$UNINSTALL org.lineageos.overlay.accent.green
-$UNINSTALL org.lineageos.overlay.accent.orange
-$UNINSTALL org.lineageos.overlay.accent.pink
-$UNINSTALL org.lineageos.overlay.accent.purple
-$UNINSTALL org.lineageos.overlay.accent.red
-$UNINSTALL org.lineageos.overlay.accent.yellow
+$UNINSTALL org.lineageos.jelly
 $UNINSTALL org.lineageos.overlay.customization.navbar.nohint
 $UNINSTALL org.lineageos.overlay.font.lato
-$UNINSTALL org.lineageos.overlay.font.rubik
+$UNINSTALL org.lineageos.profiles
 $UNINSTALL org.lineageos.recorder
 $UNINSTALL org.lineageos.updater
-$UNINSTALL org.lineageos.jelly
 
 # SET RESOLUTION
 adb shell wm density 160
 
-set SET "adb shell settings put"
 # ENABLE SOME SETTINGS
+set SET adb shell settings put
 $SET put secure ui_night_mode 2 # DARK MODE
 $SET put global airplane_mode_on 1 # FLIGHT MODE
 $SET put global policy_control immersive.full=* # IMMERSIVE MODE
@@ -156,7 +168,7 @@ cd ~/.local/share/applications
 echo NoDisplay=true | tee -a waydroid.fr.neamar.kiss.desktop waydroid.com.android.documentsui.desktop waydroid.com.android.settings.desktop waydroid.org.lineageos.jelly.desktop
 echo NoDisplay=true | tee -a waydroid.com.android.camera2.desktop waydroid.com.android.contacts.desktop waydroid.com.android.calculator2.desktop waydroid.com.android.deskclock.desktop waydroid.com.android.inputmethod.latin.desktop waydroid.com.android.gallery3d.desktop waydroid.com.android.email.desktop
 
-set BROWSER="chromium"
+set BROWSER chromium
 # INSTALL APPS
 $BROWSER 'https://github.com/keymapperorg/KeyMapper/releases/'
 $BROWSER 'https://github.com/ankidroid/Anki-Android/releases'
@@ -171,7 +183,15 @@ $BROWSER 'https://f-droid.org/en/packages/at.bitfire.davdroid/'
 ###################
 
 #  WARNING: Service manager /dev/binder has died
-Append "psi=1" to kernel parameter
+  Append `psi=1` to kernel parameter
 
 # Failed to start Clipboard manager service
-Install python-pyclip
+  Install `python-pyclip`
+
+# No sound
+  * Install `pipewire-pulse`
+  * Enable `pipewire-pulse.service`
+  
+# No video playback
+  * Install `linux-zen-header`
+  * Install `anbox-modules-dkms`
